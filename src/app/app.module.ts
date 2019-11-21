59a85fdac3fd4e8c0e5220a2cba2c4cfc582f42b
dragos-dumitriu <57215730+dragos-dumitriu@users.noreply.github.com>
2019-11-21 12:03:46 +0200

    workshop-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,40 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 import { NoteComponent } from './note/note.component';
 import { ToolsComponent } from './tools/tools.component';
-import {MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatCardModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { FilterComponent } from './filter/filter.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes:Routes=[
+  { path: "", component: HomeComponent, pathMatch:'full' },
+  { path: "add-note", component: AddNoteComponent},
+{ path: '**', redirectTo: ''}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NoteComponent,
     ToolsComponent,
-    FilterComponent
+    FilterComponent,
+    AddNoteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatCardModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
+  notes: Note[]= [
+    {
+      id: "Id1",
+      title: "First note",
+      description: "This is the description for the first note"
+    },
+    {
+      id: "Id1",
+      title: "Second note",
+      description: "This is the description for the second note"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export interface Note {
+	id: string;
+  title: string;
+ 	description: string;
 }
